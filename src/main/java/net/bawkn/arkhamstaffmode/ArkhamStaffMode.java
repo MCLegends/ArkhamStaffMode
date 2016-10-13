545a8c69c8145d107b758a6aea98a7c6cbbec728
Tux <tux2mc@gmail.com>
2016-10-12 20:40:42 -0400

    Initial commit

---
+++ src/main/java/net/bawkn/arkhamstaffmode/ArkhamStaffMode.java
@@ -0,0 +1,11 @@
+package net.bawkn.arkhamstaffmode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ArkhamStaffMode extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+}
